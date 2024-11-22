95cfee10caf7f4c17db64ef01bcd402791aa78a2
Kabilar Gunalan <kabi@mit.edu>
2024-11-22 10:12:16 -0600

    Deployed a625ede with MkDocs version: 1.5.3

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -14,7 +14,9 @@ for path in sorted(src.rglob("*.py")):
     parts = tuple(module_path.parts)
 
     if parts[-1] == "__init__":
+        print('-------',parts)
         parts = parts[:-1]
+        print(parts)
     elif parts[-1] == "__main__":
         continue
 
@@ -26,5 +28,5 @@ for path in sorted(src.rglob("*.py")):
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open(root / "docs/api/SUMMARY.md", "w") as nav_file: 
+with mkdocs_gen_files.open(root / "docs/api/navigation.md", "w") as nav_file: 
     nav_file.writelines(nav.build_literate_nav())
