8da8a29ec5448d9a88b4f6596c00de201a587c3e
Kabilar Gunalan <kabi@mit.edu>
2025-01-16 14:28:50 -0600

    Deployed 4c3d74a with MkDocs version: 1.6.1

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -9,10 +9,10 @@ nav = mkdocs_gen_files.Nav()
 root = Path(__file__).parent.parent.parent
 src = root / "linc_convert"
 
-nav['api']='api.md'
-nav['welcome']='index.md'
-nav['contribute']='contribute.md'
-nav['about']='about.md'
+nav['Welcome']='index.md'
+nav['Contribute']='contribute.md'
+nav['About']='about.md'
+nav['API']='api/api.md'
 
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
@@ -33,5 +33,5 @@ for path in sorted(src.rglob("*.py")):
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open(root / "docs/api.md", "w") as nav_file:
+with mkdocs_gen_files.open(root / "docs/api/api.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
