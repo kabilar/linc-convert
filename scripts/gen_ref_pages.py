ca994f517ed10c9aa171a164a1c5fc73aaa08bf4
Kabilar Gunalan <kabi@mit.edu>
2025-01-16 08:51:37 -0600

    Deployed b9c0897 with MkDocs version: 1.6.1

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -8,13 +8,13 @@ nav = mkdocs_gen_files.Nav()
 
 root = Path(__file__).parent.parent.parent
 src = root / "linc_convert"
-
+print(root, src)
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path(root, "docs/api", doc_path)
+    doc_path = "api" / path.relative_to(src).with_suffix(".md")
+    full_doc_path = Path(root, "docs", doc_path)
     parts = tuple(module_path.parts)
-
+    print(module_path,doc_path,full_doc_path,parts)
     if parts[-1] == "__init__":
         parts = parts[:-1]
     elif parts[-1] == "__main__":
@@ -22,17 +22,11 @@ for path in sorted(src.rglob("*.py")):
 
     if parts:
         nav[parts] = Path(doc_path).as_posix()
-        print(Path(doc_path).as_posix())
         with mkdocs_gen_files.open(full_doc_path, "w") as fd:
             ident = ".".join(parts)
             fd.write(f"::: linc_convert.{ident}")
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open(root / "docs/api/navigation.md", "w") as nav_file:
-    print(nav.build_literate_nav())
+with mkdocs_gen_files.open(root / "docs/navigation.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
-    
-with open(root / "docs/api/navigation.md", "r") as f:
-    contents = f.read()
-    print(contents)
