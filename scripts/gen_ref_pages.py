3d3f821538df2505368f7b056f248be0a0147aaf
Kabilar Gunalan <kabi@mit.edu>
2025-01-16 11:44:23 -0600

    Deployed b9c0897 with MkDocs version: 1.6.1

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -17,18 +17,19 @@ for path in sorted(src.rglob("*.py")):
     print(module_path,doc_path,full_doc_path,parts)
     if parts[-1] == "__init__":
         parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-        full_doc_path = full_doc_path.with_name("index.md")
+        # doc_path = doc_path.with_name("index.md")
+        # full_doc_path = full_doc_path.with_name("index.md")
     elif parts[-1] == "__main__":
         continue
 
     if parts:
+        print(parts, Path(doc_path).as_posix())
         nav[parts] = Path(doc_path).as_posix()
         with mkdocs_gen_files.open(full_doc_path, "w") as fd:
             ident = ".".join(parts)
             fd.write(f"::: linc_convert.{ident}")
 
-        mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
+    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
 with mkdocs_gen_files.open(root / "docs/api.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
